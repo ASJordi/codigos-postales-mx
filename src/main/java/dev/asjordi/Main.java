269f46320cce06532c7240e7915b2babd337ebe3
Jordi Ayala <jord7as@gmail.com>
2025-07-25 16:19:58 -0600

    feat: Setup logger

--- src/main/java/dev/asjordi/Main.java
+++ src/main/java/dev/asjordi/Main.java
@@ -6,32 +6,36 @@ import dev.asjordi.analytics.PostalCodeSpecification;
 import dev.asjordi.analytics.StateSpecification;
 import dev.asjordi.model.PostalCode;
 import dev.asjordi.service.Analyzer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main( String[] args ) {
 
+        logger.atInfo().log("Starting Postal Code Analyzer...");
         var analyzer = Analyzer.getInstance();
 
         var postalCode = analyzer.findFirst(new PostalCodeSpecification("50000"));
+        logger.atDebug().log("Postal Code Found: " + postalCode);
         var postalCodeByState = analyzer.find(new StateSpecification("Aguascalientes"));
+        logger.atDebug().log("Postal Codes Found by State: " + postalCodeByState);
         var postalCodeByCity = analyzer.find(new CitySpecification("Toluca de Lerdo"));
+        logger.atDebug().log("Postal Codes Found by City: " + postalCodeByCity);
 
         var postalCodeByStateAndCity = analyzer.find(
                 new AndSpecification<>(new StateSpecification("México"), new CitySpecification("Toluca de Lerdo"))
         );
+        logger.atDebug().log("Postal Codes Found by State and City: " + postalCodeByStateAndCity);
 
         var totalStates = analyzer.getCountDistinct(PostalCode::getNombreEntidad);
+        logger.atDebug().log("Total States: " + totalStates);
         var totalCities = analyzer.getCountDistinct(PostalCode::getNombreCiudad);
+        logger.atDebug().log("Total Cities: " + totalCities);
         var totalPostalCodes = analyzer.getCountDistinct(PostalCode::getCpAsentamiento);
-
-        System.out.println("Postal Code 51906: " + postalCode.orElse(null));
-        System.out.println("Postal Codes in Aguascalientes: " + postalCodeByState.size());
-        System.out.println("Postal Codes in Toluca de Lerdo: " + postalCodeByCity.size());
-        System.out.println("Postal Codes in Mexico, Toluca de Lerdo: " + postalCodeByStateAndCity.size());
-        System.out.println("Total distinct states: " + totalStates);
-        System.out.println("Total distinct cities: " + totalCities);
-        System.out.println("Total distinct postal codes: " + totalPostalCodes);
+        logger.atDebug().log("Total Postal Codes: " + totalPostalCodes);
 
     }
 
--- src/main/java/dev/asjordi/service/Analyzer.java
+++ src/main/java/dev/asjordi/service/Analyzer.java
@@ -16,7 +16,6 @@ public class Analyzer {
 
     private Analyzer() {
         this.reader = new PostalCodeDataLoader();
-//         this.reader = new PostalCodeRepository();
         init();
     }
 
